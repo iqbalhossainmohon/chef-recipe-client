25aad16ea09b0d558f3277643a0d831e438e0e69
iqbalhossainmohon <iqbalhossainmohon3057@gmail.com>
2023-05-02 13:14:35 +0600

    navbar responsivve and home banner added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -4,7 +4,13 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <h1>This is Blog page</h1>
+            <h1>1. Tell us the differences between uncontrolled and controlled components.</h1>
+
+            <h1>2. How to validate React props using PropTypes</h1>
+
+            <h1>3. Tell us the difference between nodejs and express js.</h1>
+
+            <h1>4. What is a custom hook, and why will you create a custom hook?</h1>
         </div>
     );
 };
